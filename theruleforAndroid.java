c79029413f1fe1a986c68388e58aef3b6d3b57c5
wenyuling <605536834@qq.com>
2019-03-05 09:00:41 +0800

    ThreadPool

--- theruleforAndroid.java
+++ theruleforAndroid.java
@@ -1663,9 +1663,393 @@
 　　	e.Lock可以提高多个线程进行读操作的效率。
 　　	在性能上来说，如果竞争资源不激烈，两者的性能是差不多的，而当竞争资源非常激烈时（即有大量线程同时竞争），此时Lock的性能要远远优于synchronized。所以说，在具体使用时要根据适当情况选择。
 		
-		
 	4.线程同步
+	为何要使用线程同步:
+	java允许多线程并发控制，当多个线程同时操作一个可共享的资源变量时（如数据的增删改查）， 
+    将会导致数据不准确，相互之间产生冲突，因此加入同步锁以避免在该线程没有完成操作之前，被其他线程的调用， 
+    从而保证了该变量的唯一性和准确性。
+	  1).同步方法块
+		即有synchronized关键字修饰的方法。 
+		由于java的每个对象都有一个内置锁，当用此关键字修饰方法时， 
+		内置锁会保护整个方法。在调用该方法前，需要获得内置锁，否则就处于阻塞状态。
+		代码如： 
+		public synchronized void save(){}
+		注： synchronized关键字也可以修饰静态方法，此时如果调用该静态方法，将会锁住整个类
+		public static synchronized save(){}
+	  2).同步代码块
+		即有synchronized关键字修饰的语句块。 
+		被该关键字修饰的语句块会自动被加上内置锁，从而实现同步
+		代码如： 
+		synchronized(object){ 
+		}
+		注：同步是一种高开销的操作，因此应该尽量减少同步的内容。 
+		通常没有必要同步整个方法，使用synchronized代码块同步关键代码即可。 
+	  3).使用特殊域变量(volatile)实现线程同步
+		a.volatile关键字为域变量的访问提供了一种免锁机制， 
+		b.使用volatile修饰域相当于告诉虚拟机该域可能会被其他线程更新， 
+		c.因此每次使用该域就要重新计算，而不是使用寄存器中的值 
+		d.volatile不会提供任何原子操作，它也不能用来修饰final类型的变量 
+		意思就是哪个变量需要同步操作，这个字段就修饰他
+		Demo：
+		public class SynchronizedTest {
+
+		  class Bank {
+			//需要同步的变量加上volatile
+			private volatile int account = 100;
+
+			public int getAccount() {
+			  return account;
+			}
+
+			/**
+			 * 用同步方法实现
+			 *
+			 * @param money
+			 */
+			public void save(int money) {
+			  account += money;
+			}
+
+		  }
+
+		  class NewThread implements Runnable {
+			private Bank bank;
+
+			public NewThread(Bank bank) {
+			  this.bank = bank;
+			}
+
+			@Override
+			public void run() {
+			  for (int i = 0; i < 1000; i++) {
+				//bank.save1(10);
+				bank.save(10);
+				System.out.println(i + "账户余额为：" + bank.getAccount() + " " + System.currentTimeMillis() / 1000);
+			  }
+			}
+
+		  }
+
+		  /**
+		   * 建立线程，调用内部类
+		   */
+		  public void useThread() {
+			Bank bank = new Bank();
+			NewThread new_thread = new NewThread(bank);
+			System.out.println("线程1");
+			Thread thread1 = new Thread(new_thread);
+			thread1.start();
+			System.out.println("线程2");
+			Thread thread2 = new Thread(new_thread);
+			thread2.start();
+		  }
+
+		  public static void main(String[] args) {
+			SynchronizedTest st = new SynchronizedTest();
+			st.useThread();
+		  }
+
+		}
+
+		注：多线程中的非同步问题主要出现在对域的读写上，如果让域自身避免这个问题，则就不需要修改操作该域的方法。 
+		用final域，有锁保护的域和volatile域可以避免非同步的问题。 
+		
+	  4).使用重入锁实现线程同步
+		ReentrantLock() : 创建一个ReentrantLock实例 
+        lock() : 获得锁 
+        unlock() : 释放锁 
+		注：ReentrantLock()还有一个可以创建公平锁的构造方法，但由于能大幅度降低程序运行效率，不推荐使用 
+		Demo见上Lock里的ReentrantLock
+	  5).使用局部变量实现线程同步 
+		如果使用ThreadLocal管理变量，则每一个使用该变量的线程都获得该变量的副本， 
+		副本之间相互独立，这样每一个线程都可以随意修改自己的变量副本，而不会对其他线程产生影响。
+		ThreadLocal 类的常用方法
+		ThreadLocal() : 创建一个线程本地变量 
+		get() : 返回此线程局部变量的当前线程副本中的值 
+		initialValue() : 返回此线程局部变量的当前线程的"初始值" 
+		set(T value) : 将此线程局部变量的当前线程副本中的值设置为value
+		Demo：
+		public class SynchronizedTest {
+
+		  public class Bank{
+			//使用ThreadLocal类管理共享变量account
+			private ThreadLocal<Integer> account = new ThreadLocal<Integer>(){
+			  @Override
+			  protected Integer initialValue(){
+				return 100;
+			  }
+			};
+			public void save(int money){
+			  account.set(account.get()+money);
+			}
+			public int getAccount(){
+			  return account.get();
+			}
+		  }
+
+		  class NewThread implements Runnable {
+			private Bank bank;
+
+			public NewThread(Bank bank) {
+			  this.bank = bank;
+			}
+
+			@Override
+			public void run() {
+			  for (int i = 0; i < 1000; i++) {
+				//bank.save1(10);
+				bank.save(10);
+				System.out.println(i + "账户余额为：" + bank.getAccount() + " " + System.currentTimeMillis() / 1000);
+			  }
+			}
+
+		  }
+
+		  /**
+		   * 建立线程，调用内部类
+		   */
+		  public void useThread() {
+			Bank bank = new Bank();
+			NewThread new_thread = new NewThread(bank);
+			System.out.println("线程1");
+			Thread thread1 = new Thread(new_thread);
+			thread1.start();
+			System.out.println("线程2");
+			Thread thread2 = new Thread(new_thread);
+			thread2.start();
+		  }
+
+		  public static void main(String[] args) {
+			SynchronizedTest st = new SynchronizedTest();
+			st.useThread();
+		  }
+
+		}
+		
+		注：ThreadLocal与同步机制 
+        a.ThreadLocal与同步机制都是为了解决多线程中相同变量的访问冲突问题。 
+        b.前者采用以"空间换时间"的方法，后者采用以"时间换空间"的方式
+		
+	  6).使用阻塞队列实现线程同步
+		LinkedBlockingQueue<E>来实现线程的同步 
+		LinkedBlockingQueue<E>是一个基于已连接节点的，范围任意的blocking queue。 
+		队列是先进先出的顺序（FIFO），关于队列以后会详细讲解~ 
+	    LinkedBlockingQueue 类常用方法 
+		LinkedBlockingQueue() : 创建一个容量为Integer.MAX_VALUE的LinkedBlockingQueue 
+		put(E e) : 在队尾添加一个元素，如果队列满则阻塞 
+		size() : 返回队列中的元素个数 
+		take() : 移除并返回队头元素，如果队列空则阻塞 
+		
+	    代码实例： 
+			实现商家生产商品和买卖商品的同步
+		
+		Demo:
+		public class BlockingSynchronizedThread {
+		  /**
+		   * 定义一个阻塞队列用来存储生产出来的商品
+		   */
+		  private LinkedBlockingQueue<Integer> queue = new LinkedBlockingQueue<Integer>();
+		  /**
+		   * 定义生产商品个数
+		   */
+		  private static final int size = 10;
+		  /**
+		   * 定义启动线程的标志，为0时，启动生产商品的线程；为1时，启动消费商品的线程
+		   */
+		  private int flag = 0;
+
+		  private class LinkBlockThread implements Runnable {
+			@Override
+			public void run() {
+			  int new_flag = flag++;
+			  System.out.println("启动线程 " + new_flag);
+			  if (new_flag == 0) {
+				for (int i = 0; i < size; i++) {
+				  int b = new Random().nextInt(255);
+				  System.out.println("生产商品：" + b + "号");
+				  try {
+					queue.put(b);
+				  } catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				  }
+				  System.out.println("仓库中还有商品：" + queue.size() + "个");
+				  try {
+					Thread.sleep(100);
+				  } catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				  }
+				}
+			  } else {
+				for (int i = 0; i < size / 2; i++) {
+				  try {
+					int n = queue.take();
+					System.out.println("消费者买去了" + n + "号商品");
+				  } catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				  }
+				  System.out.println("仓库中还有商品：" + queue.size() + "个");
+				  try {
+					Thread.sleep(100);
+				  } catch (Exception e) {
+					// TODO: handle exception
+				  }
+				}
+			  }
+			}
+		  }
+
+		  public static void main(String[] args) {
+			BlockingSynchronizedThread bst = new BlockingSynchronizedThread();
+			LinkBlockThread lbt = bst.new LinkBlockThread();
+			Thread thread1 = new Thread(lbt);
+			Thread thread2 = new Thread(lbt);
+			thread1.start();
+			thread2.start();
+
+		  }
+		}
+	  
+		注：BlockingQueue<E>定义了阻塞队列的常用方法，尤其是三种添加元素的方法，我们要多加注意，当队列满时：
+	　　add()方法会抛出异常
+	　　offer()方法返回false
+	　　put()方法会阻塞
+	  7).使用原子变量实现线程同步
+		需要使用线程同步的根本原因在于对普通变量的操作不是原子的。
+		那么什么是原子操作呢？
+		原子操作就是指将读取变量值、修改变量值、保存变量值看成一个整体来操作
+		即-这几种行为要么同时完成，要么都不完成。
+		在java的util.concurrent.atomic包中提供了创建了原子类型变量的工具类，
+		使用该类可以简化线程同步。
+		其中AtomicInteger 表可以用原子方式更新int的值，可用在应用程序中(如以原子方式增加的计数器)，
+		但不能用于替换Integer；可扩展Number，允许那些处理机遇数字类的工具和实用工具进行统一访问。
+		AtomicInteger类常用方法：
+		AtomicInteger(int initialValue) : 创建具有给定初始值的新的AtomicInteger
+		addAddGet(int dalta) : 以原子方式将给定值与当前值相加
+		get() : 获取当前值
+		上面的Demo替换一下Bank
+		class Bank {
+			private AtomicInteger account = new AtomicInteger(100);
+
+			public AtomicInteger getAccount() {
+			  return account;
+			}
+
+			public void save(int money) {
+			  account.addAndGet(money);
+			}
+		  }
+	  
+		补充--原子操作主要有：
+	　　对于引用变量和大多数原始变量(long和double除外)的读写操作；
+	　　对于所有使用volatile修饰的变量(包括long和double)的读写操作。
+	  
 	5.线程池
+	ThreadPool
+	  1).什么是线程池
+	  为了避免系统频繁的创建和销毁线程带来的性能消耗，可以让线程得到很好的复用。当需要使用线程时从线程池取（原来是创建），当用完时归还线程池（原来是销毁）。
+	  2)JDK中的线程池
+	  通过Executors类方法可以看出，
+	  a.它的方法ExecutorService newFixedThreadPool(int nThreads)可以创建固定数量的线程池；
+	  Demo:
+	  public class FixThreadPoolDemo {
+
+		  public static class MyTask implements Runnable{
+
+			@Override public void run() {
+			  System.out.println(System.currentTimeMillis() + "Thread Name:" + Thread.currentThread().getName());
+
+			  try {
+				Thread.sleep(1000);
+			  } catch (InterruptedException e) {
+				e.printStackTrace();
+			  }
+			}
+		  }
+
+		  public static void main(String[] args){
+			MyTask myTask = new MyTask();
+			int size =5;
+			ExecutorService es = Executors.newFixedThreadPool(size);
+			for (int i = 0; i < 10 ; i++) {
+			  es.submit(myTask);
+			}
+
+		  }
+		}
+
+	  b.方法ExecutorService newSingleThreadExecutor()可以创建只有一个线程的线程池；
+	  
+		public static void main(String[] args){
+			MyTask myTask = new MyTask();
+			ExecutorService es = Executors.newSingleThreadExecutor();
+			for (int i = 0; i < 10 ; i++) {
+			  es.submit(myTask);
+			}
+		  }
+		  
+	  c.方法ExecutorService newCachedThreadPool() 可以返回一个根据实际情况调整线程数量的线程池；
+	  public static void main(String[] args){
+		MyTask myTask = new MyTask();
+		ExecutorService es = Executors.newCachedThreadPool();
+		for (int i = 0; i < 10 ; i++) {
+		  es.submit(myTask);
+		}
+	  }
+	  
+	  d.方法ScheduledExecutorService newSingleThreadScheduledExecutor()可以返回一个可以执行定时任务线程的线程池；
+	  OS可以拿来执行延迟定时任务
+	  public static void main(String[] args){
+		MyTask myTask = new MyTask();
+		ExecutorService es = Executors.newSingleThreadScheduledExecutor();
+		try {
+		  es.awaitTermination(5, TimeUnit.SECONDS);
+		  for (int i = 0; i < 10 ; i++) {
+			es.submit(myTask);
+		  }
+		} catch (InterruptedException e) {
+		  e.printStackTrace();
+		}
+	  }
+	  
+	  e.方法ScheduledExecutorService newScheduledThreadPool(int corePoolSize)可以返回指定数量可以执行定时任务的线程池。
+	  当然这些方法都有重载方法，例如参数中加入自定义的ThreadFacotry等。
+	
+	  public static void main(String[] args){
+		ScheduledExecutorService executorService = Executors.newScheduledThreadPool(5);
+		/**
+		 * 1,executorService.scheduleAtFixedRate:创建一个周期性任务，从上个任务开始，过period周期执行下一个（如果执行时间>period，则以执行时间为周期）
+		 * 2,executorService.scheduleWithFixedDelay：创建一个周期上午，从上个任务结束，过period周期执行下一个。
+		 */
+		//如果前边任务没有完成则调度也不会启动
+		executorService.scheduleAtFixedRate(new Runnable() {
+		  public void run() {
+			try {
+			  Thread.sleep(1000);
+			  System.out.println("当前时间：" + System.currentTimeMillis()/1000);
+			} catch (InterruptedException e) {
+			  e.printStackTrace();
+			}
+		  }
+		},0,2, TimeUnit.SECONDS);
+	  }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	6.JUC库
   五.IO
   六.网络编程
